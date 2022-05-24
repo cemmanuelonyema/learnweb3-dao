7a8148f127fab45ecebb2f191987ee1ad694d008
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-24 02:04:24 +0100

    Working

---
+++ hardhat/constants/index.js
@@ -0,0 +1,8 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x0528dF383b215248B25030F94b41196dDC46F3dd";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+// FakeNFTMarketplace deployed to:  0x076E4614b1AE4751088C86BAE6291E5DfCD3aa19
+// CryptoDevsDAO deployed to:  0x27a74fe8Cc2C91A77aEe89c90De1903048Ae0324
